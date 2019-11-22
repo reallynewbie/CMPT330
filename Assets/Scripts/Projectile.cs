aedb8a7bd68d3665e3cb82ade7ea3e55028acc23
luckycham <48453186+luckycham@users.noreply.github.com>
2019-11-21 22:20:17 -0700

    Shooting a fireball
    Fire1 button now initiates a fireball from spawn location. has a firing cooldown time. SampleScene has a demo of it.

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Moves projectile and deals damage to player
+/// </summary>
+/// 
+/// Source: https://www.youtube.com/watch?v=wkKsl1Mfp5M
+/// 
+/// Field           Description
+/// speed           Speed of the projectile
+/// 
+/// Author: Chamod Welhenge
+/// 
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    public float speed = 5f;
+    public float damage = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the rigidbody of bullet
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+    }
+
+
+
+    /// <summary>
+    /// Deals damage to object it hits
+    /// </summary>
+    /// <param name="collision">Object being collided with</param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.transform.name);
+    }
+
+}
